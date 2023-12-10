847b39111ecf7480d62f8f03cf34765265c4bd7f
Nathan Tomer <nathan.tomer@gmail.com>
2023-12-11 11:59:58 +1300

    add normal vectors

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,26 @@
 #include "vec3.hpp"
 #include "ray.hpp"
 
+double hit_sphere(const point3& center, double radius, const ray& r){
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius*radius;
+    auto distriminant = b*b - 4*a*c;
+
+    if (distriminant < 0)
+        return -1;
+    else
+        return (-b - sqrt(distriminant) ) / (2.0*a);
+}
+
 color ray_color(const ray& r){
+    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
+    if (t>0.0){
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    }
+
     vec3 unit_dir = unit_vector(r.direction());
     auto a = 0.5*(unit_dir.y() +1.0);
     return (1-a)*color(1,1,1) + a*color(0.5, 0.7, 1.0);
