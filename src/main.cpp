f9a9ffc485522415c2857633ade38230bb8353da
Nathan Tomer <nathan.tomer@gmail.com>
2023-12-11 12:04:41 +1300

    Simplify hit_sphere

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,15 @@
 
 double hit_sphere(const point3& center, double radius, const ray& r){
     vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius*radius;
-    auto distriminant = b*b - 4*a*c;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius*radius;
+    auto distriminant = half_b*half_b - a*c;
 
     if (distriminant < 0)
         return -1;
     else
-        return (-b - sqrt(distriminant) ) / (2.0*a);
+        return (-half_b - sqrt(distriminant) ) / a;
 }
 
 color ray_color(const ray& r){
