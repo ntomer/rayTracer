5ff55e38d6df0e453231306a77300a6b25e08180
Nathan Tomer <nathan.tomer@gmail.com>
2023-12-12 10:21:04 +1300

    Add interval class

---
+++ src/interval.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <limits>
+
+class interval {
+    public:
+    double min, max;
+
+    //default interval is empty
+    interval() : min(std::numeric_limits<double>::infinity()), 
+                    max(-std::numeric_limits<double>::infinity()) {}
+
+    interval(double _min, double _max) : min(_min), max(_max) {}
+
+    bool contains(double x) const{
+        return min <= x && x <= max;
+    }
+
+    bool surrounds(double x) const {
+        return min < x && x < max;
+    }
+
+    static const interval empty, universe;
+
+};
+
+const static interval empty(std::numeric_limits<double>::infinity(), 
+                            -std::numeric_limits<double>::infinity());
+const static interval w(-std::numeric_limits<double>::infinity(), 
+                            std::numeric_limits<double>::infinity());
