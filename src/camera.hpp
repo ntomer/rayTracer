8c2fa2b2d663514fdc60de9971e46239f09e2adc
Nathan Tomer <nathan.tomer@gmail.com>
2023-12-12 16:39:35 +1300

    no stack overflow

--- src/camera.hpp
+++ src/camera.hpp
@@ -11,6 +11,7 @@ class camera {
     double aspect_ratio_ideal = 1.0;
     int im_width = 100;
     int samples_per_pixel = 10;
+    int max_depth = 10; 
 
     void render (const hittable& world) {
         initialize();
@@ -23,7 +24,7 @@ class camera {
                 color pixel_color(0,0,0);
                 for(int sample = 0; sample < samples_per_pixel; sample++){
                     ray r = get_ray(i,j);
-                    pixel_color += ray_color(r, world);
+                    pixel_color += ray_color(r, max_depth, world);
                 }
 
                 write_color(std::cout, pixel_color,samples_per_pixel);
@@ -85,13 +86,15 @@ class camera {
         return (px * pixel_delta_u) + (py * pixel_delta_v);
     }
 
-    color ray_color(const ray& r, const hittable& world) const {
+    color ray_color(const ray& r, int depth, const hittable& world) const {
         hit_record rec;
+        if (depth <= 0)
+            return color(0,0,0);
 
         if (world.hit(r, interval(0,infinity), rec)) {
             //return 0.5 * (rec.normal + color(1,1,1));
             vec3 direction = random_on_hemisphere(rec.normal);
-            return 0.5 * ray_color(ray(rec.p, direction), world);
+            return 0.5 * ray_color(ray(rec.p,  direction), depth-1, world);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
