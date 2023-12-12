b19afa1b3550f209ebdd5dc1351f14f2350e5aa1
Nathan Tomer <nathan.tomer@gmail.com>
2023-12-12 16:00:01 +1300

    antialiasing by square pixel sampling

--- src/color.hpp
+++ src/color.hpp
@@ -6,8 +6,20 @@
 
 using color = vec3;
 
-void write_color(std::ostream &out, color pixel_color) {
-    out << static_cast<int>(255.999 * pixel_color.x()) << ' '
-        << static_cast<int>(255.999 * pixel_color.y()) << ' '
-        << static_cast<int>(255.999 * pixel_color.z()) << '\n';
+
+//accepts the sum of the color(0.0-1.0), with the number of samples
+// calculates the average, scales the result to 8 bit, then outputs it to the stream;
+void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
+
+    auto scale = 1.0 / samples_per_pixel;
+    pixel_color *= scale;
+
+    static const interval intensity(0.000, 0.999);
+    auto r = intensity.clamp(pixel_color.x());
+    auto g = intensity.clamp(pixel_color.y());
+    auto b = intensity.clamp(pixel_color.z());
+
+    out << static_cast<int>(256 * r) << ' '
+        << static_cast<int>(256 * g) << ' '
+        << static_cast<int>(256 * b) << '\n';
 }
--- src/rtweekend.hpp
+++ src/rtweekend.hpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <cstdlib>
 
 
 // Usings
@@ -20,6 +21,15 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+    return rand() / (RAND_MAX +1.0);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max-min)*random_double();
 }
 
 // Common Headers
