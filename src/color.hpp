d207bc2a9a2dd3170acfeee9cab92e8a5a2e7d42
Nathan Tomer <nathan.tomer@gmail.com>
2023-12-12 17:03:12 +1300

    gamma correction

--- src/color.hpp
+++ src/color.hpp
@@ -6,6 +6,10 @@
 
 using color = vec3;
 
+inline double linear_to_gamma(double linear_component)
+{
+    return sqrt(linear_component);
+}
 
 //accepts the sum of the color(0.0-1.0), with the number of samples
 // calculates the average, scales the result to 8 bit, then outputs it to the stream;
@@ -15,9 +19,9 @@ void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
     pixel_color *= scale;
 
     static const interval intensity(0.000, 0.999);
-    auto r = intensity.clamp(pixel_color.x());
-    auto g = intensity.clamp(pixel_color.y());
-    auto b = intensity.clamp(pixel_color.z());
+    auto r = intensity.clamp(linear_to_gamma(pixel_color.x()));
+    auto g = intensity.clamp(linear_to_gamma(pixel_color.y()));
+    auto b = intensity.clamp(linear_to_gamma(pixel_color.z()));
 
     out << static_cast<int>(256 * r) << ' '
         << static_cast<int>(256 * g) << ' '
